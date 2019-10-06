e4c141667ea448e5882a5acdd22a26123f7df386
Anu Thomas Chandy <anuamd@hotmail.com>
2019-10-06 10:28:06 -0700

    Adding UI elements (button, TextView)

--- bazel-android/contoso-app/src/main/java/com/contoso/app/MainActivity.java
+++ bazel-android/contoso-app/src/main/java/com/contoso/app/MainActivity.java
@@ -1,14 +1,23 @@
 package com.contoso.app;
 
 import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
-import com.contoso.common.CommonHelper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    private Button okButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sample_main);
+        okButton = (Button) findViewById(R.id.button_ok);
+    }
+
+    public void onOkClick(View view) {
+       TextView textView = (TextView) findViewById(R.id.textView_result);
+       textView.setText("got click event!");
     }
 }
